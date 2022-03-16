c2b22bcc76699183f625ef1ca4bdb6599fd04fab
wingram1 <waingram96@gmail.com>
2022-03-15 19:27:21 -0500

    set up handlebars

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const routes = require('./routes');
-const sequelize = require('./config/connection');
+const express = require("express");
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,11 +9,19 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// handlebars setup
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, "public")));
+
 // turn on routes
 app.use(routes);
 
 // urn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
-  });
-  
+  app.listen(PORT, () => console.log("Now listening"));
+});
